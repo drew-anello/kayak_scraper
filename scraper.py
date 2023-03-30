404e78956c3419ec8579ea30dfd14763955cee11
Drew Anello <drew.anello@gmail.com>
2023-03-30 11:51:39 -0400

    updates location adds URL

--- scraper.py
+++ scraper.py
@@ -3,3 +3,9 @@ from time import sleep
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
+
+driver = webdriver.Chrome()
+to_location = 'MIA'
+url = "https://www.kayak.com/flights/NYC-{to_location}/2023-04-20/2023-04-30?sort=bestflight_a".format(
+    to_location=to_location)
